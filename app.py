d59b0066fa994e16f173d7f4d617aca922be0bb8
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-05 21:06:37 -0500

    scrap first bootstrap template

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template 
-app = Flask(__name__) 
- 
-@app.route('/') 
-def hello_world(): 
-    return render_template('index.html')
- 
-if __name__ == '__main__': 
-    app.run() 
+from flask import Flask, send_file, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/resume")
+def show_resume():
+    return render_template("resume.html")
+
+@app.route("/portfolio")
+def show_portfolio():
+    return render_template("portfolio-details.html")
+
+@app.route("/blog")
+def show_blog():
+    return render_template("blog-single.html")
+if __name__ == "__main__":
+    app.run(debug=True)
