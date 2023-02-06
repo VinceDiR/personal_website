d102c8faf1ff719b47c15f0aa0635380afaf808f
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-05 21:09:26 -0500

    add folders

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,17 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/resume")
-def show_resume():
-    return render_template("resume.html")
-
-@app.route("/portfolio")
-def show_portfolio():
-    return render_template("portfolio-details.html")
-
-@app.route("/blog")
-def show_blog():
-    return render_template("blog-single.html")
-if __name__ == "__main__":
-    app.run(debug=True)
