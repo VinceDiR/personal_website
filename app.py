456622129f83e65b9115e39e4fd31f80d624ba46
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-06 21:55:03 -0500

    update

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/blog")
-def show_post(post_id):
-    return render_template("blog-single.html")
-
-@app.route("/portfolio")
-def show_portfolio():
-    return render_template("portfolio-details.html")
-
 @app.route("/resume")
 def show_resume():
     return render_template("resume.html")
