6a7224bd80250bb5815a7fcd3bd0538be5880d9e
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-09 23:33:54 -0500

    add dockerfile, edit app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""Docstring for app.py."""
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,8 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Docstring for index."""
     return render_template("index.html")
 
 @app.route("/resume")
 def show_resume():
+    """Docstring for show_resume."""
     return render_template("resume.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5002))
+    app.run(debug=True, host='0.0.0.0', port=port)
