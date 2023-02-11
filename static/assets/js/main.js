77d61371b8cbce4135d8885c815416930a125dc6
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-11 11:21:41 -0500

    update index, js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -174,12 +174,6 @@
     });
   }
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
 
   /**
    * Testimonials slider
