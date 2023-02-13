aed39ee22e03038e285bba75ac3072fba4aba2d3
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-13 15:43:43 -0500

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 """Docstring for app.py."""
 import os
+import subprocess
+from dotenv import load_dotenv
 from flask import Flask, render_template
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+load_dotenv()
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
 
@@ -15,6 +18,11 @@ def index():
 def show_resume():
     """Docstring for show_resume."""
     return render_template("resume.html")
+
+#@app.route("/forms/contact.php", methods=["POST"])
+#def contact():
+#    """Docstring for contact."""
+#    subprocess.run("forms/contact.php", check=False)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5002))
