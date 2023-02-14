90460c6ce2b471d939601f90a6323315ee956e06
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-13 22:06:25 -0500

    update

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 """Docstring for app.py."""
 import os
-import subprocess
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 load_dotenv()
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
+
+app.config['MAIL_SERVER']= os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('EMAIL_PORT')
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PW')
+app.config['MAIL_USE_TLS'] = True
+
+mail = Mail(app)
 
 @app.route("/")
 def index():
@@ -19,10 +27,18 @@ def show_resume():
     """Docstring for show_resume."""
     return render_template("resume.html")
 
-#@app.route("/forms/contact.php", methods=["POST"])
-#def contact():
-#    """Docstring for contact."""
-#    subprocess.run("forms/contact.php", check=False)
+@app.route("/contact", methods=["POST"])
+def contact():
+    """Docstring for contact."""
+    contact_name = request.form["name"]
+    contact_email = request.form["email"]
+    contact_subject = request.form["subject"]
+    contact_message = request.form["message"]
+
+    msg = Message(f"{contact_name} - {contact_subject}", sender=contact_email, recipients=[os.environ.get('EMAIL')])
+    msg.body = contact_message
+    mail.send(msg)
+    return "Success!"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5002))
