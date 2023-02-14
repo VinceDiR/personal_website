bdb3db8c961281092c7fd2c9f25253818f92786c
Nate <88605064+NateDiR@users.noreply.github.com>
2023-02-14 15:04:59 -0500

    update app

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 """Docstring for app.py."""
 import os
+import requests
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-from werkzeug.middleware.proxy_fix import ProxyFix
+
+
 
 load_dotenv()
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
+
 
 app.config['MAIL_SERVER']= os.environ.get('MAIL_SERVER')
 app.config['MAIL_PORT'] = os.environ.get('EMAIL_PORT')
@@ -35,10 +37,25 @@ def contact():
     contact_subject = request.form["subject"]
     contact_message = request.form["message"]
 
-    msg = Message(f"{contact_name} - {contact_subject}", sender=contact_email, recipients=[os.environ.get('EMAIL')])
-    msg.body = contact_message
-    mail.send(msg)
-    return "Success!"
+    response = requests.get(
+    os.environ.get('EMAIL_API_ENDPOINT'),
+    params = {'email': contact_email},
+    headers= {'Authorization': os.environ.get('EMAIL_API_KEY')}, timeout=5)
+
+    status = response.json()['status']
+
+    if status == "valid":
+        msg = Message(f"Website Inquiry: {contact_subject}", sender=contact_email, recipients=[os.environ.get('EMAIL')])
+        msg.body = f"""
+        From: {contact_name} <{contact_email}>
+        Subject: {contact_subject}
+        Body: {contact_message}
+        """
+        mail.send(msg)
+        return "Message sent"
+    if status == "invalid":
+        return "Invalid email address"
+    return None
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5002))
